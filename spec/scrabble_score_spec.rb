3d6bb8fe211481249afb3815a4d9340dd7719bf2
Maldon Meehan and Matt Wilkin <maldon@>
2016-06-28 11:26:56 -0700

    add passing code for the letter K

--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -17,6 +17,11 @@ describe('Sting#scrabble_score') do
   it("returns a scrabble score for a letter") do
     expect("b".scrabble_score()).to(eq(3))
   end
+
+  it("returns a scrabble score for a letter") do
+    expect("k".scrabble_score()).to(eq(5))
+  end
+
 end
 
 
