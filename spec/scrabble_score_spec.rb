7ebecfda15018a08f543fa0f7739b0466ec61042
Maldon Meehan and Matt Wilkin <maldon@>
2016-06-28 11:31:31 -0700

    add passing code for letters that score 10

--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -25,11 +25,13 @@ describe('Sting#scrabble_score') do
   it("returns a scrabble score for a letter") do
     expect("j".scrabble_score()).to(eq(8))
   end
+
+  it("returns a scrabble score for a letter") do
+    expect("q".scrabble_score()).to(eq(10))
+  end
 end
 
 
 
-# | If word has letters: "K"  |"K"| 5 |
-# | If word has letters: "J, X"  |"X"| 8 |
 # | If word has letters: "Q, Z"  |"Z"| 10 |
 # | Program add the letter values and returns a score to the user  |"CAT"| 3+1+1 = 5 |
